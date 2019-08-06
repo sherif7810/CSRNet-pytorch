76ebf8a473eb68bc73b802ec5cd67a6b15e39867
Sherif Ali Ahmad Mohamad <she7631@hotmail.com>
2019-08-06 12:20:18 +0200

    Remove Cropping Augmentation
    - The condition's always false.
    - I expect an index-out-of-bounds error can happen in case `dx` or `dy` + `cropsize` equals the width or height.

--- image.py
+++ image.py
@@ -1,10 +1,9 @@
 import random
-import os
-from PIL import Image,ImageFilter,ImageDraw
+from PIL import Image
 import numpy as np
 import h5py
-from PIL import ImageStat
 import cv2
+
 
 def load_data(img_path, train = True):
     gt_path = img_path.replace('.jpg', '.h5').replace('images', 'ground_truth')
@@ -12,25 +11,7 @@ def load_data(img_path, train = True):
     gt_file = h5py.File(gt_path)
     target = np.asarray(gt_file['density'])
     if train:
-        crop_size = (img.size[0]/2,img.size[1]/2)
-        if random.randint(0,9)<= -1:
-            
-            
-            dx = int(random.randint(0,1)*img.size[0]*1./2)
-            dy = int(random.randint(0,1)*img.size[1]*1./2)
-        else:
-            dx = int(random.random()*img.size[0]*1./2)
-            dy = int(random.random()*img.size[1]*1./2)
-        
-        
-        
-        img = img.crop((dx,dy,crop_size[0]+dx,crop_size[1]+dy))
-        target = target[dy:crop_size[1]+dy,dx:crop_size[0]+dx]
-        
-        
-        
-        
-        if random.random()>0.8:
+        if random.random() > 0.8:
             target = np.fliplr(target)
             img = img.transpose(Image.FLIP_LEFT_RIGHT)
 
